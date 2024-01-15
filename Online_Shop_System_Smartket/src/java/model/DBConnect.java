d2c28a2f0a160471445d1603d1922bbd27d61c09
minhtt <minhtthe273206@fpt.edu.vn>
2024-01-15 23:47:35 +0700

    update cart use db

---
+++ Online_Shop_System_Smartket/src/java/model/DBConnect.java
@@ -0,0 +1,44 @@
+package model;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class DBConnect {
+        Connection conn = null;
+    public DBConnect(String url, String user, String pass){
+         try {
+                //call driver
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                //connect
+                conn=DriverManager.getConnection(url, user, pass);
+                System.out.println("connected");
+            } catch (ClassNotFoundException | SQLException ex) {
+                Logger.getLogger(DBConnect.class.getName()).log(Level.SEVERE, null, ex);
+            }
+    }
+    public DBConnect() {
+        this("jdbc:mysql://localhost:3306/restaurant", "root", "12345678");
+    }
+    public ResultSet getData(String sql){
+        ResultSet rs = null;
+        Statement state;
+        try {
+            state = conn.createStatement(
+                    ResultSet.TYPE_SCROLL_SENSITIVE,
+                    ResultSet.CONCUR_UPDATABLE);
+            rs = state.executeQuery(sql);
+        }catch (SQLException ex) {
+            
+        }
+        return rs;
+    }
+    public static void main(String[] args) {
+        new DBConnect();
+    }
+}
