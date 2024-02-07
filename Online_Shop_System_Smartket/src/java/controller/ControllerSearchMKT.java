8cfae1310853247d0699470c4e2a2fd3e74c1614
hungnnhe173202 <hungngongocfpt@gmail.com>
2024-02-07 20:32:28 +0700

    add search

--- Online_Shop_System_Smartket/src/java/controller/ControllerSearchMKT.java
+++ Online_Shop_System_Smartket/src/java/controller/ControllerSearchMKT.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package controller;
 
 import java.io.IOException;
@@ -12,41 +11,59 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.sql.ResultSet;
+import java.util.Vector;
+import model.DAOProduct;
+import view.Product;
 
 /**
  *
  * @author HP
  */
-@WebServlet(name="ControllerSearchMKT", urlPatterns={"/ControllerSearchMKTURL"})
+@WebServlet(name = "ControllerSearchMKT", urlPatterns = {"/ControllerSearchMKTURL"})
 public class ControllerSearchMKT extends HttpServlet {
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
+        try ( PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ControllerSearchMKT</title>");  
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ControllerSearchMKT at " + request.getContextPath () + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+            DAOProduct dao = new DAOProduct();
+            String indexPage = request.getParameter("index");
+            if (indexPage == null) {
+                indexPage = "1";
+            }
+            int index = Integer.parseInt(indexPage);
+            index = (index - 1) * 10;
+            String keyWord = request.getParameter("keyWord");
+            ResultSet rs = dao.getData(
+                    "SELECT * FROM product as p join productImage as pi on p.ProductID = pi.ProductID\n"
+                    + " WHERE LOWER(p.ProductName) LIKE N'%"+ keyWord + "%' and pi.ProductURL like '%_1%'"
+                            + " limit 10 offset " + index);
+            int count = dao.getTotalProduct();
+            int endPage = count / 10;
+            if (count % 10 != 0) {
+                endPage++;
+            }
+            request.setAttribute("data", rs);
+            request.setAttribute("endP", endPage);
+            request.getRequestDispatcher("productListmkt.jsp").forward(request, response);
         }
-    } 
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -54,12 +71,13 @@ public class ControllerSearchMKT extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
-    } 
+    }
 
-    /** 
+    /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -67,12 +85,13 @@ public class ControllerSearchMKT extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override
--- Online_Shop_System_Smartket/web/productListmkt.jsp
+++ Online_Shop_System_Smartket/web/productListmkt.jsp
@@ -18,7 +18,7 @@
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-        <<link rel="stylesheet" href="css/css_saleProductList/saleProductList.css"/>
+        <link rel="stylesheet" href="css/css_saleProductList/saleProductList.css"/>
         <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
@@ -45,11 +45,13 @@
                     <div class="table-filter">
                         <div class="row">
                             <div class="col-sm-9">
-                                <button type="button" class="btn btn-primary"><i class="fa fa-search"></i></button>
-                                <div class="filter-group">
-                                    <label>Tìm kiếm</label>
-                                    <input type="text" class="form-control">
-                                </div>
+                                <form action="ControllerSearchMKTURL" method="post">
+                                    <button type="submit" value="search" class="btn btn-primary"><i class="fa fa-search"></i></button>
+                                    <div class="filter-group">
+                                        <label>Tìm kiếm</label>
+                                        <input type="text" name="keyWord" class="form-control" placeholder="Tìm kiếm">
+                                    </div>
+                                </form>
                                 <div class="filter-group">
                                     <label>Loại</label>
                                     <select class="form-control">
